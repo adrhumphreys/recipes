7167e36acc4b2aefdbac576ab309dc35de42e53d
Adrian Humphreys <adrian.humphreys@silverstripe.com>
2020-05-06 20:27:30 +1200

    yeet

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,17 @@ module.exports = {
   siteMetadata: {
     title: `Recipes`,
     description: `Good food`,
-    author: `adrhumphreys`,
+    author: `adrhumphreys`
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -32,17 +33,17 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-offline`,
       options: {
         // precachePages: [`/about-us/`, `/projects/*`],
-      },
-    },
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
